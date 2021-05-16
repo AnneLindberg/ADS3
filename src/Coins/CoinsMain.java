abbc652aec7108770fb6cef16bdb5bac8cc42db0
Anne Lindberg <anne.lindberg@ambiant.io>
2021-05-16 12:43:40 +0200

    added coins main. Now figues out how many coins you need, however not the specific number of specific coins

---
+++ src/Coins/CoinsMain.java
@@ -0,0 +1,44 @@
+package Coins;
+
+public class CoinsMain {
+
+    public static void main(String[] args) {
+        int coins[] = {1,7,10,22};
+        int sum = 100;
+
+        CoinsMain main = new CoinsMain();
+        System.out.println("You need at least " + main.coinChange(coins, sum) + " coins to to make a value of " + sum);
+    }
+
+    public int coinChange(int[] coins, int sum){
+        int totalCoins = coins.length;
+        double[][] arr = new double[totalCoins + 1][sum + 1];
+        for(int j = 0; j <= sum; j++){
+            arr[0][j] = Double.POSITIVE_INFINITY;
+        }
+        for(int i = 1; i <= totalCoins; i++){
+            arr[i][0] = 0;
+        }
+
+        for(int i = 1; i <= totalCoins; i++){
+            for(int j = 1; j <= sum; j++){
+                if(coins[i - 1] <= j)
+                    arr[i][j] = min(1 + arr[i][j - coins[i - 1]], arr[i - 1][j]);
+                else
+                    arr[i][j] = arr[i - 1][j];
+            }
+        }
+
+        return (int)arr[totalCoins][sum];
+    }
+
+    private double min(double v, double v1) {
+        if(v <= v1){
+            return v;
+        }
+        else{
+            return v1;
+        }
+    }
+}
+
--- src/nQueens/QueensMain.java
+++ src/nQueens/QueensMain.java
@@ -1,3 +1,5 @@
+package nQueens;
+
 import java.util.Scanner;
 
 public class QueensMain {

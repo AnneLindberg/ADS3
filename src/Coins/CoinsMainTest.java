a7cc07dc7f87b7b4d86037a11b157d25e7e8915c
Sonny <Sonnyhaack@gmail.com>
2021-05-16 16:50:02 +0200

    Test setup.. Men what to do?

---
+++ src/Coins/CoinsMainTest.java
@@ -0,0 +1,24 @@
+package Coins;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoinsMainTest {
+
+    @BeforeEach
+    void setUp()
+        {
+        }
+
+    @Test
+    void main()
+        {
+        }
+
+    @Test
+    void coinChange()
+        {
+        }
+}
---
+++ src/nQueens/QueensMainTest.java
@@ -0,0 +1,31 @@
+package nQueens;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueensMainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp()
+        {
+           int[][] chess = new int[4][4];
+           String qsf = "";
+           int row = 0;
+        }
+
+    @org.junit.jupiter.api.Test
+    void main()
+        {
+
+        }
+
+    @org.junit.jupiter.api.Test
+    void printQueens()
+        {
+
+        }
+
+    @org.junit.jupiter.api.Test
+    void isSafeToPlaceQueen()
+        {
+        }
+}
